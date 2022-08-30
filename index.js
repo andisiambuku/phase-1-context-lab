de244a800ad9215df235ac2d1b52fc70b51b1bf7
andisiambuku <roselandambuku@gmail.com>
2022-08-30 11:43:24 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -21,3 +21,70 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(worker){
+    return{
+        firstName:worker[0],
+        familyName:worker[1],
+        title: worker[2],
+        payPerHour:worker[3],
+        timeInEvents:[],
+        timeOutEvents:[],
+    }
+}
+
+function createEmployeeRecords(workerArrays){
+    return workerArrays.map(worker => createEmployeeRecord(worker))
+}
+
+function createTimeInEvent(workerObj,timeInStamp){
+    let [date, time] = timeInStamp.split(' ')
+    workerObj.timeInEvents.push({
+        type:"TimeIn",
+        hour:parseInt(time,10),
+        date:date
+    })
+    return workerObj;
+}
+
+function createTimeOutEvent(workerObj,timeOutStamp){
+    let [date, time] = timeOutStamp.split(' ')
+    workerObj.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(time,10),
+        date:date,
+    })
+    return workerObj;
+}
+
+function hoursWorkedOnDate(workerObj,date){
+    let timeIn = workerObj.timeInEvents.find(x => x.date === date)
+    let timeOut = workerObj.timeInEvents.find(x => x.date === date)
+    let totalHoursWorked = (timeOut.hour - timeIn.hour) /100
+
+    return totalHoursWorked;
+    
+}
+
+function wagesEarnedOnDate(workerObj,date){
+    return hoursWorkedOnDate(workerObj,date) * workerObj.payPerHour
+}
+
+function allWagesFor(workerObj){
+    let datesTheyworked = workerObj.timeInEvents.map(x=> x.date)
+    let wageEarnedPerDate = datesTheyworked.map(date => wagesEarnedOnDate(workerObj,date))
+    return wageEarnedPerDate.reduce((sum,wage) => sum +wage, 0)
+    
+}
+
+function calculatePayroll(workerArrays){
+    let payroll = 0;
+    for(const wkr of workerArrays){
+        payroll += allWagesFor(wkr)
+    }
+    return payroll;
+    
+}
+
+
+
+
